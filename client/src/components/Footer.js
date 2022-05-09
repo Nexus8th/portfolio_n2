a8fab039064b38ba8128fb18fd492e62d0ee71a8
Julien <mmos002@gmail.com>
2022-05-09 17:55:27 +0200

    2nd push

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+// Footer including Logo, Shortened Navlinks, Socials Links
+
+import React from 'react'
+
+
+
+function Footer() {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+// Header including Navbar, Navlinks, Logo for the whole
+
+import React from 'react'
+
+
+function Header() {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
