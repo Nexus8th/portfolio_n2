9147714c58c52539ac8e85090659df3002450091
Julien <mmos002@gmail.com>
2022-05-18 17:59:43 +0200

    Working on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,41 @@ import Exercices from './components/Pages/Exercices';
 import Projets from './components/Pages/Projets';
 import Contact from './components/Pages/Contact';
 import Dashboard from './components/Pages/Dashboard';
+import { UidContext } from './components/Context/AppContext';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function App() {
+
+  const [uid, setUid] = useState(null)
+
+  useEffect(() => {
+    const fetchToken = async() => {
+      await axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}jwtid`,
+        withCredentials: true
+      })
+      .then((res) => setUid(res.data))
+      .catch((err) => console.log("No token"))
+    }
+    fetchToken()
+  }, [uid])
+
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/signup' element={<Signup/>}></Route>
-        <Route path='/exercices' element={<Exercices/>}></Route>
-        <Route path='/projets' element={<Projets/>}></Route>
-        <Route path='/contact' element={<Contact/>}></Route>
-        <Route path='/dashboard' element={<Dashboard/>}></Route>
-      </Routes>
-    </Router>
+    <UidContext.Provider value={uid}>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/signup' element={<Signup/>}></Route>
+          <Route path='/exercices' element={<Exercices/>}></Route>
+          <Route path='/projets' element={<Projets/>}></Route>
+          <Route path='/contact' element={<Contact/>}></Route>
+          <Route path='/dashboard' element={<Dashboard/>}></Route>
+        </Routes>
+      </Router>
+    </UidContext.Provider>
   );
 }
 
--- client/src/components/Pages/Login.js
+++ client/src/components/Pages/Login.js
@@ -3,11 +3,42 @@
 import Footer from '../Footer/Footer'
 import Header from '../Header/Header'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import axios from 'axios'
 
 function Login() {
 
-  const navigate = useNavigate()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
+  const handleLogin = (e) => {
+    e.preventDefault()
+    const emailError = document.querySelector(".login-email-error")
+    const passwordError = document.querySelector(".login-password-error")
+
+    axios({
+      method: "post",
+      url: `${process.env.REACT_APP_API_URL}api/user/login`,
+      withCredentials: true,
+      data: {
+        email, password
+      }
+    })
+    .then((res) => {
+      if (res.data.errors) {
+        emailError.innerHTML = res.data.errors.email
+        passwordError.innerHTML = res.data.errors.password
+      } else {
+        window.location = '/'
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  // Navigation path to SignUp component
+  const navigate = useNavigate()
   const signupRoute = (e) => {
     e.preventDefault()
     let path = '/signup'
@@ -17,12 +48,20 @@ function Login() {
   return (
     <>
     <Header/>
-    <form className="login-form" action="" method="get">
+    <form className="login-form" action="" method="get" onSubmit={handleLogin}>
       <h1 className="login-h1">Connexion</h1>
       <label className="login-label" htmlFor="email">Email</label>
-      <input className="login-input" type="email" name="email" required></input>
+      <input className="login-input" type="text" name="email" required onChange={(e) => setEmail(e.target.value)} value={email}></input>
+
+      {/* Handling email errors messages display */}
+      <div className="login-email-error"></div>
+
       <label className="login-label" htmlFor="password">Mot de passe</label>
-      <input className="login-input" type="password" name="password" required></input>
+      <input className="login-input" type="password" name="password" onChange={(e) => setPassword(e.target.value)} value={password}></input>
+
+      {/* Handling password errors messages display */}
+      <div className="login-password-error"></div>
+
       <button className="login-btn" type="submit">Connexion</button>
     </form>
     <p className="login-p">Nouveau? <a className="login-a" href="# " onClick={signupRoute}>Inscrivez-vous!</a></p>
