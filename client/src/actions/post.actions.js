7ce7b00932c68437564256573cba6eae74b5d7c5
Julien <mmos002@gmail.com>
2022-06-03 15:18:26 +0200

    Fixed post order, now on creating codepen's API integration

--- client/src/actions/post.actions.js
+++ client/src/actions/post.actions.js
@@ -12,7 +12,7 @@ export const getPosts = (num) => {
     return axios
       .get(`${process.env.REACT_APP_API_URL}api/post/`)
       .then((res) => {
-        const array = res.data.slice(0, num)
+        const array = res.data.reverse(0, num)
         dispatch({ type: GET_POSTS, payload: array });
         dispatch({ type: GET_ALL_POSTS, payload: res.data })
       })
