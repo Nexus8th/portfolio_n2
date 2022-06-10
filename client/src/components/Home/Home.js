ba49e3128b79c95206fd08a033efedc2b6e018de
Julien <mmos002@gmail.com>
2022-06-10 17:57:58 +0200

    Homepage added Terminal alike thingy

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -17,8 +17,16 @@ function Home() {
         className="home-background-video">
       </video> */}
       <Header />
-      <div className="home-chat-container">
-
+      <div className="home-container">
+        <div className="home-chat-container">
+          <div className="home-chat-top-bar">
+            <span className="home-chat-top-bar-dot-1"></span>
+            <span className="home-chat-top-bar-dot-2"></span>
+            <span className="home-chat-top-bar-dot-3"></span>
+            <span className="home-chat-top-bar-title">Terminal</span>
+          </div>
+          <input className="home-chat-input"></input>
+        </div>
       </div>
       <Footer />
     </>
